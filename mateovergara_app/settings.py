5dffeded2a312ecaf1f4b699da61fc20322b7981
mavergarah <mavergarah@gmail.com>
2025-01-07 16:37:25 -0500

    changes added in settings.py in databases. Localhost changed by postgres

--- mateovergara_app/settings.py
+++ mateovergara_app/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'NAME': 'mateovergara_sql',
         'USER': 'mavergarah',
         'PASSWORD': 'S4F3RL1N1B4',
-        'HOST': 'localhost',
+        'HOST': 'postgres',
         'PORT': '5433',
     }
 }
